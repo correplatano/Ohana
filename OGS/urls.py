ce05818befa71dd5d1b9cd3d94aaa5169b15ed76
correplatano <n.perlado.h@gmail.com>
2020-04-30 13:39:51 +0200

    khjli

--- OGS/urls.py
+++ OGS/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import url
 
-from OGS.views import first_view, SobreNosotrosView
+from OGS.views import first_view, SobreNosotrosView, CafeView, BebidasCalientesView, BebidasFriasView, SnacksView, \
+    ServiciosView, PromocionesView, ContactoView
 
 urlpatterns = [
     url(r'^$', first_view, name='vending-ogs'),
     url(r'^sobrenosotros/$', SobreNosotrosView.as_view(), name='sobre-nosotros'),
+    url(r'^cafe/$', CafeView.as_view(), name='cafe'),
+    url(r'^productos/bebidascalientes/$', BebidasCalientesView.as_view(), name='bebidas-calientes'),
+    url(r'^productos/bebidasfrias/$', BebidasFriasView.as_view(), name='bebidas-frias'),
+    url(r'^productos/snacks/$', SnacksView.as_view(), name='snacks'),
+    url(r'^servicios/$', ServiciosView.as_view(), name='servicios'),
+    url(r'^promociones/$', PromocionesView.as_view(), name='promociones'),
+    url(r'^contacto/$', ContactoView.as_view(), name='contacto'),
 
 ]
--- OGS/views.py
+++ OGS/views.py
@@ -17,3 +17,38 @@ class SobreNosotrosView(View):
     def get(self, request):
         return render(request, 'sobrenosotros.html')
 
+
+class CafeView(View):
+    def get(self, request):
+        return render(request, 'cafe.html')
+
+
+class BebidasCalientesView(View):
+    def get(self, request):
+        return render(request, 'bebidas-calientes.html')
+
+
+class BebidasFriasView(View):
+    def get(self, request):
+        return render(request, 'bebidas-frias.html')
+
+
+class SnacksView(View):
+    def get(self, request):
+        return render(request, 'snacks.html')
+
+
+class ServiciosView(View):
+    def get(self, request):
+        return render(request, 'servicios.html')
+
+
+class PromocionesView(View):
+    def get(self, request):
+        return render(request, 'promociones.html')
+
+
+class ContactoView(View):
+    def get(self, request):
+        return render(request, 'contacto.html')
+
